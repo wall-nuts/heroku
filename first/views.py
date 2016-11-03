501845a03bb995e9ef7e3317789a0d27c0035be6
wall-nuts <448769943@qq.com>
2016-11-03 19:55:05 +0800

    setting.py   INSTALLED_APPS need  a "," at last

--- first/views.py
+++ first/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-#coding:utf-8
+# coding:utf-8
 from django.http import HttpResponse
+
+
 def index(request):
     return HttpResponse(u"huanyingguangling")
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -12,7 +12,6 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', first_views.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', first_views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
